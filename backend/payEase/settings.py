adfc1c7536b209cd0142a438cf4a39f3abb8caf4
vishalkarnati <vishalkarnati9@gmail.com>
2026-07-01 00:23:35 +0530

    feat: initialize project settings and create home landing page template

--- backend/payEase/settings.py
+++ backend/payEase/settings.py
@@ -152,3 +152,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+WHITENOISE_USE_FINDERS = True
